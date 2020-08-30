37be75e600f4a4850dce768e41209bdb009f3257
Karoline Alencar <karolinedalencar@gmail.com>
2020-08-30 15:11:10 -0300

    add @reach/router to project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router, Link } from "@reach/router";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <nav>
+      <Link to="/">Home</Link> 
+    </nav>
+    <Router>
+      <Home path="/" />
+    </Router>
+  </div>
+);
 
-export default App;
+const Home = () => (
+  <div>
+    <h2>Welcome</h2>
+  </div>
+);
+
+export default App
