388296c7e791fe868cb765253fa6b64a5d1232a1
Karoline Alencar <karolinedalencar@gmail.com>
2020-09-01 19:09:31 -0300

    add form validation on login

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,23 +1,44 @@
 import React from "react";
+import { useForm } from 'react-hook-form';
 
 import Illustration from '../illustration'
 
-const Login = () => (
-    <main className="container">
-        <Illustration />
-        <h1>Login <span role="img" aria-label="Victory Hand">✌️</span></h1>
-        <form>
-            <label>
-                <span>Username</span>
-                <input type="text" placeholder="ex: soraka" />
-            </label>
-            <label>
-                <span>Password</span>
-                <input type="password" />
-            </label>
-            <button className="action">Login</button>
-        </form>
-    </main>
-);
+const Login = () => {
+    const { register, handleSubmit, errors } = useForm();
+
+    const onSubmit = data => {
+        const { username, password } = data
+        console.log(username, password)
+    }
+
+    return (        
+        <main className="container">
+            <Illustration />
+            <h1>Login <span role="img" aria-label="Victory Hand">✌️</span></h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>
+                    <p>Username</p>
+                    <input 
+                        name="username" 
+                        type="text" 
+                        placeholder="ex: soraka" 
+                        ref={register({ required: true })}
+                    />
+                    { errors.username && <span>Username is required.</span> }
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input 
+                        name="password" 
+                        type="password" 
+                        ref={register({ required: true })}
+                    />
+                    {errors.password && <span>Password is required.</span> }
+                </label>
+                <button className="action">Login</button>
+            </form>
+        </main>
+    )
+};
 
 export default Login
